3b5c88ca3dcccb02dc5b3e4567b10be2fa86b782
CNT_RASPBERRY1 <cnt.generaloperations@gmail.com>
2019-10-03 23:40:50 +0530

    final code as of 3 oct 2019

--- motion.py
+++ motion.py
@@ -3,7 +3,12 @@ from time import sleep
 import numpy as np
 import io
 import sys
+from datetime import datetime as dtime
 
+
+def get_datestamp():
+    return str(dtime.now().__format__("%d-%m-%Y")) + str(".h264")
+    
 def rgb2gray(img):
     return np.dot(img[...,:3],[0.299,0.587,0.114])
     ##  convert rgb image to gray by dot product with constants 
@@ -27,7 +32,7 @@ def write_to_file(stream,filename):
     ##  write the whole h264 video object to given file(in append binary mode)
         
 
-def main(filename='default_clip.h264',preview="no",resolution='320x240',length_of_recording=10,number_of_pixels=45,threshold=28):
+def main(preview="no",filename=get_datestamp(),resolution='320x240',length_of_recording=10,number_of_pixels=50,threshold=30):
     res = str(resolution)
     res_list = list(map(int,res.split('x')))
     if(res_list[0] and res_list[1]):
@@ -46,13 +51,14 @@ def main(filename='default_clip.h264',preview="no",resolution='320x240',length_o
     with picamera.PiCamera() as camera:
         camera.resolution = (resolution_c,resolution_r)
         camera.framerate = 20
+        camera.annotate_text_size = 12
         output_stream = picamera.PiCameraCircularIO(camera,seconds=length_of_recording)
         sleep(1)
         ##  setup camera
         
         print("captured background")
         background = np.empty((resolution_r,resolution_c,3),dtype=np.uint8)
-        camera.capture(background,'rgb')
+        camera.capture(background,'rgb',use_video_port=True)
         background_gray = rgb2gray(background)
         ##  get background
 
@@ -66,7 +72,7 @@ def main(filename='default_clip.h264',preview="no",resolution='320x240',length_o
                 camera.start_preview()
             while(True):
                 pixels = 0
-                camera.capture(img_color,'rgb')
+                camera.capture(img_color,'rgb',use_video_port = True)
                 img_gray = rgb2gray(img_color)
                 diff = np.absolute(background_gray-img_gray)
 
@@ -80,11 +86,22 @@ def main(filename='default_clip.h264',preview="no",resolution='320x240',length_o
                     print("Motion detected")
                     output_stream.clear()
                     camera.start_recording(output_stream,format='h264')
-                    camera.wait_recording(length_of_recording)
+                    start = dtime.now()
+
+                    while( (dtime.now() - start).seconds < length_of_recording):
+                        camera.annotate_text = dtime.now().__format__("%H:%M:%S-%d/%m/%Y")
+                        camera.wait_recording(1)
+                        
                     camera.stop_recording()
                     write_to_file(output_stream,filename)
+
+                    camera.capture(img_color,'rgb',use_video_port = True)
+                    img_gray = rgb2gray(img_color)
                 ##  if number of pixels is large enough record video and save
 
+####Added
+#                else:
+                    background_gray = (img_gray)                     
                      
         finally:
             if(preview == "preview"):
@@ -94,17 +111,17 @@ def main(filename='default_clip.h264',preview="no",resolution='320x240',length_o
 
 if __name__ == "__main__":
     if(len(sys.argv) == 7):
-        main(filename=sys.argv[1],preview=sys.argv[2],resolution=sys.argv[3],length_of_recording=sys.argv[4],number_of_pixels=sys.argv[5],threshold=sys.argv[6])
+        main(preview=sys.argv[1],filename=sys.argv[2],resolution=sys.argv[3],length_of_recording=sys.argv[4],number_of_pixels=sys.argv[5],threshold=sys.argv[6])
     elif(len(sys.argv) == 6):
-        main(filename=sys.argv[1],preview=sys.argv[2],resolution=sys.argv[3],length_of_recording=sys.argv[4],number_of_pixels=sys.argv[5])
+        main(preview=sys.argv[1],filename=sys.argv[2],resolution=sys.argv[3],length_of_recording=sys.argv[4],number_of_pixels=sys.argv[5])
     elif(len(sys.argv) == 5):
-        main(filename=sys.argv[1],preview=sys.argv[2],resolution=sys.argv[3],length_of_recording=sys.argv[4])
+        main(preview=sys.argv[1],filename=sys.argv[2],resolution=sys.argv[3],length_of_recording=sys.argv[4])
     elif(len(sys.argv) == 4):
-        main(filename=sys.argv[1],preview=sys.argv[2],resolution=sys.argv[3])
+        main(preview=sys.argv[1],filename=sys.argv[2],resolution=sys.argv[3])
     elif(len(sys.argv) == 3):
-        main(filename=sys.argv[1],preview=sys.argv[2])
+        main(preview=sys.argv[1],filename=sys.argv[2])
     elif(len(sys.argv) == 2):
-        main(filename=sys.argv[1])
+        main(preview=sys.argv[1])
     else:
         main()
     
